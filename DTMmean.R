a8931c9bcf74615b226d561f89788a9883e37760
Vincent Delbar <vincent.delbar@gmail.com>
2024-07-11 21:38:23 +0200

    Update DTMmean.R

--- DTMmean.R
+++ DTMmean.R
@@ -1,36 +1,33 @@
 #!/usr/bin/env Rscript
-library(tools)
 library(stringr)
 # Terra env
 suppressPackageStartupMessages(library(terra))
 library(terra)
 terraOptions(progress=0)
 
-########## Mean smooth DTM in a 25m window ##########
-radius <- 12.5
-method <- "sum"
-
 smooth <- function(path) {
-  # Read input data and NA value
+  library(terra)
+  ######## Mean smooth DTM in a 25m window ##########
   dtm <- terra::rast(path)
-  nodata <- grep("NoData Value", describe(path), value = TRUE)
-  nodata <- strsplit(na, "=")[[1]][2] |> as.numeric()
   
-  # Prepare kernel
+  # Prepare output
+  nodata <- grep("NoData Value", terra::describe(path), value = TRUE)
+  nodata <- strsplit(na, "=")[[1]][2] |> as.numeric()
+  outfile <- paste(file_path_sans_ext(path), "_smooth.tif", sep = "")
+  gdalopt <- c("COMPRESS=ZSTD", "ZLEVEL=1", "PREDICTOR=3", "TILED=YES", "BIGTIFF=IF_SAFER")
+  
+  # Create kernel
+  radius <- 12.5
   kernel <- terra::focalMat(dtm, radius, "circle")
+  method <- "sum" # or "max
   if (method == "max") {
     kernel[which(kernel != 0)] <- 1
   }
   
-  # Apply fun and directly write output
-  outfile <- paste(file_path_sans_ext(path), "_smooth.tif", sep = "")
-  copt <- c("COMPRESS=ZSTD", "ZLEVEL=1", "PREDICTOR=3", "TILED=YES", "BIGTIFF=IF_SAFER")
-  
-  # Use faster focal CPP implementation
-  terra::focalCpp(dtm, kernel, method, fill_value=nodata, filename=outfile, wopt=list(copt))
+  # Use faster C++ implementation of "terra::focal"
+  terra::focalCpp(dtm, kernel, method, fill_value=nodata, filename=outfile, wopt=list(gdalopt))
 }
 
-args <- commandArgs(trailingOnly = TRUE)
-for (f in args) {
+for (f in commandArgs(trailingOnly = TRUE)) {
   smooth(f)
 }
