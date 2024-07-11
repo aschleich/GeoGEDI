2f2e564e947950fc28b1ebf898ea3b9086f4f7ab
Vincent Delbar <vincent.delbar@gmail.com>
2024-07-11 16:37:44 +0200

    Declare function + try garbage collect

--- DTMmean.R
+++ DTMmean.R
@@ -12,12 +12,10 @@ radius <- 12.5
 method <- "sum"
 args <- commandArgs(trailingOnly = TRUE)
 
-for (f in args) {
-  #message("Processing ", basename(f))
-
+smooth <- function(path) {
   # Read input data and NA value
-  ras <- terra::rast(f)
-  na <- grep("NoData Value", describe(f), value = TRUE)
+  ras <- terra::rast(path)
+  na <- grep("NoData Value", describe(path), value = TRUE)
   na <- strsplit(na, "=")[[1]][2] |> as.numeric()
 
   # Prepare kernel and apply smoothing
@@ -26,10 +24,16 @@ for (f in args) {
     focal_weight_matrix[which(focal_weight_matrix != 0)] <- 1
   }
   smooth_raster <- terra::focal(ras, focal_weight_matrix, fun = method)
+  terra::crs(smooth_raster) <- terra::crs(ras)
+  rm(ras)
+  gc()
 
   # Write output with GDAL create options
-  out_tif <- paste(file_path_sans_ext(f), "_smooth.tif", sep = "")
+  out_tif <- paste(file_path_sans_ext(path), "_smooth.tif", sep = "")
   copt <- c("COMPRESS=ZSTD", "ZLEVEL=1", "PREDICTOR=3", "TILED=YES", "BIGTIFF=IF_SAFER")
-  terra::crs(smooth_raster) <- terra::crs(ras)
   terra::writeRaster(smooth_raster, out_tif, overwrite = TRUE, gdal = copt, NAflag = na)
+}
+
+for (f in args) {
+  smooth(f)
 }
