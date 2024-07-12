2ce10b3b74e25e73bc4c8de00cab95e0cb3de836
Vincent Delbar <vincent.delbar@gmail.com>
2024-07-12 15:45:59 +0200

    Process only one DTM file, overwrite existing

--- DTMmean.R
+++ DTMmean.R
@@ -4,28 +4,25 @@ library(stringr)
 # Terra env
 suppressPackageStartupMessages(library(terra))
 library(terra)
-terraOptions(progress=0)
+terraOptions(progress = 0)
 
-smooth <- function(path) {
+smooth <- function(inpath, outpath) {
   ######## Mean smooth DTM in a 25m window ##########
-  outfile <- paste(file_path_sans_ext(path), "_smooth.tif", sep = "")
-  if (!file.exists(outfile)) {
-    dtm <- terra::rast(path)
-    # Prepare output
-    nodata <- -99999
-    copt <- c("COMPRESS=ZSTD", "ZLEVEL=1", "PREDICTOR=3", "TILED=YES", "BIGTIFF=IF_SAFER")
-    # Create kernel
-    radius <- 12.5
-    kernel <- terra::focalMat(dtm, radius, "circle")
-    method <- "sum" # for focal mean
-    if (method == "max") {
-      kernel[which(kernel != 0)] <- 1
-    }
-    # Apply focal function and write output
-    terra::focal(dtm, w=kernel, fun=method, filename=outfile, wopt=list(gdal=copt, NAflag=nodata))
+  dtm <- terra::rast(inpath)
+  # Prepare output
+  nodata <- -99999
+  # Create kernel
+  radius <- 12.5
+  kernel <- terra::focalMat(dtm, radius, "circle")
+  method <- "sum" # for focal mean
+  if (method == "max") {
+    kernel[which(kernel != 0)] <- 1
   }
+  # Apply focal function and write output
+  copt <- c("COMPRESS=ZSTD", "ZLEVEL=1", "PREDICTOR=3", "TILED=YES", "BIGTIFF=IF_SAFER")
+  opts <- list(gdal = copt, NAflag = nodata)
+  terra::focal(dtm, kernel, method, filename = outpath, overwrite = TRUE, wopt = opts)
 }
 
-for (f in commandArgs(trailingOnly = TRUE)) {
-  smooth(f)
-}
+arguments <- commandArgs(trailingOnly = TRUE)
+smooth(arguments[1], arguments[2])
