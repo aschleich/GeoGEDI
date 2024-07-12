f46ca99958e2c9950a9ba5f8bd9f54489bd7edd1
Vincent Delbar <vincent.delbar@gmail.com>
2024-07-12 12:27:58 +0200

    Directly write output file, with nodata value

--- DTMmean.R
+++ DTMmean.R
@@ -8,24 +8,22 @@ terraOptions(progress=0)
 
 smooth <- function(path) {
   ######## Mean smooth DTM in a 25m window ##########
-  dtm <- terra::rast(path)
-  
-  # Prepare output
-  nodata <- grep("NoData Value", terra::describe(path), value = TRUE)
-  nodata <- strsplit(nodata, "=")[[1]][2] |> as.numeric()
   outfile <- paste(file_path_sans_ext(path), "_smooth.tif", sep = "")
-  gdalopt <- c("COMPRESS=ZSTD", "ZLEVEL=1", "PREDICTOR=3", "TILED=YES", "BIGTIFF=IF_SAFER")
-  
-  # Create kernel
-  radius <- 12.5
-  kernel <- terra::focalMat(dtm, radius, "circle")
-  method <- "sum" # or "max
-  if (method == "max") {
-    kernel[which(kernel != 0)] <- 1
+  if (!file.exists(outfile)) {
+    dtm <- terra::rast(path)
+    # Prepare output
+    nodata <- -99999
+    copt <- c("COMPRESS=ZSTD", "ZLEVEL=1", "PREDICTOR=3", "TILED=YES", "BIGTIFF=IF_SAFER")
+    # Create kernel
+    radius <- 12.5
+    kernel <- terra::focalMat(dtm, radius, "circle")
+    method <- "sum" # for focal mean
+    if (method == "max") {
+      kernel[which(kernel != 0)] <- 1
+    }
+    # Apply focal function and write output
+    a <- terra::focal(dtm, w=kernel, fun=method, filename=outfile, wopt=list(gdal=copt, NAflag=nodata))
   }
-  
-  # Use faster C++ implementation of "terra::focal"
-  terra::focal(dtm, kernel, method, fill_value=nodata, filename=outfile, wopt=list(gdalopt))
 }
 
 for (f in commandArgs(trailingOnly = TRUE)) {
