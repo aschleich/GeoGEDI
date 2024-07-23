5d2df9e6861ea37b392dac09908a053c5facce7d
Vincent Delbar <vincent.delbar@gmail.com>
2024-07-23 19:53:49 +0200

    Split code in two functions

--- ExtractH5data.R
+++ ExtractH5data.R
@@ -2,35 +2,10 @@
 #---------------------------
 # Convert HDF5 GEDI data to RDS file + shift z values using geoid
 #---------------------------
-suppressPackageStartupMessages(library(bit64))
-library(bit64, warn.conflicts = FALSE)
-library(dplyr, warn.conflicts = FALSE)
+suppressPackageStartupMessages(library(library(bit64, warn.conflicts = FALSE)))
 suppressPackageStartupMessages(library(terra))
-library(terra)
+library(dplyr, warn.conflicts = FALSE)
 library(rhdf5)
-
-# Script arguments
-arguments <- commandArgs(trailingOnly = TRUE)
-
-if (! dir.exists("trash")) {
-  dir.create("trash")
-}
-datadir <- arguments[1]
-files <- paste0(datadir, "/", dir(datadir, pattern = "*.h5", recursive = TRUE))
-
-# Download IGN geoid undulation grid, converted from txt to GeoTIFF
-# See PROJ-data github repository: https://github.com/OSGeo/PROJ-data/tree/master/fr_ign
-geoid_grid_file <- arguments[2]
-
-if (startsWith(basename(geoid_grid_file), "fr_ign_RAF")) {
-  epsg_code <- "5698"
-} else if (startsWith(basename(geoid_grid_file), "fr_ign_RAC")) {
-  epsg_code <- "5699"
-} else {
-  print("Could not guess CRS based on IGN geoid grid name.")
-  quit(status = 1)
-}
-geoid_grid <- terra::rast(geoid_grid_file)
 
 quality_filter <- TRUE
 out_vector <- TRUE
@@ -59,109 +34,124 @@ variables <- c(
   "/land_cover_data/modis_treecover_sd"
 )
 
+
+process_beam <- function(h5_obj, beam_num) {
+  half_beam_names <- c("BEAM0011", "BEAM0010", "BEAM0001", "BEAM0000")
+  # Read datasets headers only
+  stru2 <- h5dump(h5_obj, recursive = 2, all = FALSE, load = FALSE)
+  beam_name <- names(stru2)[beam_num]
+
+  get_dataset <- function(ds_path) {
+    name <- paste0("h5_obj&'/", beam_name, ds_path, "'")
+    path <- eval(parse(text = name))
+    if (ds_path == "/shot_number") {
+      return(rhdf5::H5Dread(path, bit64conversion = "bit64"))
+    } else {
+      return(rhdf5::H5Dread(path))
+    }
+  }
+
+  data <- lapply(variables, get_dataset)
+  col_names <- unlist(lapply(variables, function(varname) tail(unlist(strsplit(varname, "/")), 1)))
+  for (i in 0:100) {
+    data <- append(data, list(get_dataset("/rh")[i + 1, ]))
+    col_names <- append(col_names, paste0("rh", i))
+  }
+  data <- c(beam_name, data)
+  col_names <- c("beam_name", col_names)
+  beam_df <- as.data.frame(data, col.names = col_names)
+  beam_df$power <- ifelse(beam_df$beam_name %in% half_beam_names, "half", "full")
+
+  return(beam_df)
+}
+
+process_orbit <- function(h5_file) {
+  h5_obj <- rhdf5::H5Fopen(h5_file)
+  gedi_df <- as.data.frame(do.call(process_beam(h5_obj, 1:8)))
+  h5closeAll()
+
+  gedi_df$orbit <- substr(gedi_df$shot_number, 1, 4)
+  geom_cols <- c("lon_lowestmode", "lat_lowestmode")
+  gedi_df <- vect(gedi_df, geom = geom_cols, crs = "epsg:4326")
+
+  # Transform height to altitude using geoid undulation grid
+  geoid_crs <- crs(geoid_grid)
+  suppressWarnings({
+    gedi_df$geoid_height <- extract(geoid_grid, project(gedi_df, geoid_crs))[2]
+  })
+  # h(IGN69) = h(WGS84) - h(RAF18)
+  gedi_df$elev_ngf <- gedi_df$elev_lowestmode - gedi_df$geoid_height
+
+  # Save lon/lat and x/y coords
+  gedi_df$lon <- geom(gedi_df)[, 3]
+  gedi_df$lat <- geom(gedi_df)[, 4]
+  gedi_df <- project(gedi_df, paste0("epsg:", epsg_code))
+  gedi_df$x <- geom(gedi_df)[, 3]
+  gedi_df$y <- geom(gedi_df)[, 4]
+
+  gedi_df <- as.data.frame(gedi_df)
+
+  # Keep only quality data
+  n_samples <- dim(gedi_df)[1]
+  if (quality_filter) {
+    gedi_df <- subset(gedi_df, surface_flag == "01" & degrade_flag == "00" & quality_flag == "01")
+  }
+
+  if (dim(gedi_df)[1] == 0) {
+    print("Couldn't find any good quality samples")
+  } else {
+    print(paste0("Extracted ", dim(gedi_df)[1], "/", n_samples, " samples"))
+  } # end loop rbind data to final dataframe
+
+  return(gedi_df)
+}
+
+# Script arguments
+arguments <- commandArgs(trailingOnly = TRUE)
+datadir <- arguments[1]
+files <- paste0(datadir, "/", dir(datadir, pattern = "*.h5", recursive = TRUE))
+if (! dir.exists("trash")) {
+  dir.create("trash")
+}
+
+# First download IGN's grid of geoid undulation, converted from txt to GeoTIFF
+# See PROJ-data github repository: https://github.com/OSGeo/PROJ-data/tree/master/fr_ign
+geoid_grid_file <- arguments[2]
+if (startsWith(basename(geoid_grid_file), "fr_ign_RAF")) {
+  epsg_code <- "5698"
+} else if (startsWith(basename(geoid_grid_file), "fr_ign_RAC")) {
+  epsg_code <- "5699"
+} else {
+  print("Could not guess CRS based on geoid filename.")
+  epsg_code <- readline(prompt = "Enter your EPSG number:")
+}
+geoid_grid <- rast(geoid_grid_file)
+
+# Main loop on all files (one h5 file per orbit)
 final_df <- data.frame()
 
-# Loop on all files (each file one orbit)
 for (f in files) {
   print(paste0("Processing ", f))
-
   tryCatch(
     {
-      h5_obj <- rhdf5::H5Fopen(f)
-      gedi_data <- data.frame()
-
-      # Loop all the beams of one file (=all 8 beams from one orbit)
-      for (j in 1:8) {
-        stru2 <- h5dump(h5_obj, recursive = 2, all = FALSE, load = FALSE)
-        beam_name <- names(stru2)[j]
-
-        get_dataset <- function(variable) {
-          name <- paste0("h5_obj&'/", beam_name, variable, "'")
-          path <- eval(parse(text = name))
-          if (variable == "/shot_number") {
-            return(rhdf5::H5Dread(path, bit64conversion = "bit64"))
-          } else {
-            return(rhdf5::H5Dread(path))
-          }
-        }
-
-        data <- lapply(variables, get_dataset)
-        col_names <- lapply(variables, function(varname) tail(unlist(strsplit(varname, "/")), 1))
-        col_names <- unlist(col_names)
-        # rh98 <- H5Dread(eval(parse(text=paste0("h5_obj&'/",beam_name,"/rh")[99,] #98%
-        # rh100 <- H5Dread(eval(parse(text=paste0("h5_obj&'/",beam_name,"/rh")[101,] #100%
-        # rh0 <- H5Dread(eval(parse(text=paste0("h5_obj&'/",beam_name,"/rh")[1,] #0%
-        for (i in 0:100) {
-          data <- append(data, list(get_dataset("/rh")[i + 1, ]))
-          col_names <- append(col_names, paste0("rh", i))
-        }
-        data <- c(beam_name, data)
-        col_names <- c("beam_name", col_names)
-        beam_data <- as.data.frame(data, col.names = col_names)
-        gedi_data <- rbind(gedi_data, beam_data)
-      } # end loop on beams
-
-      # From data.frame to SpatVector
-      geom_cols <- c("lon_lowestmode", "lat_lowestmode")
-      gedi_data <- terra::vect(gedi_data, geom = geom_cols, crs = "epsg:4326")
-
-      # Transform height to altitude using geoid undulation grid
-      geoid_crs <- terra::crs(geoid_grid)
-      suppressWarnings({
-        gedi_data$geoid_height <- terra::extract(geoid_grid, project(gedi_data, geoid_crs))[2]
-      })
-      # h(IGN69) = h(WGS84) - h(RAF18)
-      gedi_data$elev_ngf <- gedi_data$elev_lowestmode - gedi_data$geoid_height
-
-      # Reproject data to Lambert
-      gedi_data_proj <- terra::project(gedi_data, paste0("epsg:", epsg_code))
-
-      # Extract columns of X and Y coordinates
-      gedi_data$X_L93 <- terra::geom(gedi_data_proj)[, 3]
-      gedi_data$Y_L93 <- terra::geom(gedi_data_proj)[, 4]
-      gedi_data_proj$X_wgs <- terra::geom(gedi_data)[, 3]
-      gedi_data_proj$Y_wgs <- terra::geom(gedi_data)[, 4]
-
-      # Transform SpatialPointsDataFrame to data.frame
-      gedi_data_proj <- as.data.frame(gedi_data_proj)
-
-      # Keep only quality data
-      n_samples <- dim(gedi_data_proj)[1]
-      if (quality_filter) {
-        gedi_data_proj <- subset(gedi_data_proj, surface_flag == "01" & degrade_flag == "00" & quality_flag == "01")
-      }
-
-      if (dim(gedi_data_proj)[1] == 0) {
-        print("Couldn't find any good quality samples")
-      } else {
-        print(paste0("Extracted ", dim(gedi_data_proj)[1], "/", n_samples, " samples"))
-        final_df <- rbind(final_df, gedi_data_proj)
-      } # end loop rbind data to final dataframe
-
-      h5closeAll()
+      final_df <- rbind(final_df, process_orbit(f))
     },
+    # When H5 file is corrupted
     error = function(e) {
-      print(paste0("Failed to process ", f, ", moving to trash folder"))
+      print(paste0("Failed to process ", h5_file, ", moving to trash folder"))
       print(e)
-      file.copy(f, paste0("trash/", basename(f)))
-      file.remove(f)
+      # TODO: check error message
+      #file.copy(h5_file, paste0("trash/", basename(h5_file)))
+      #file.remove(h5_file)
     }
   )
-} # end loop for file
+}
 
-final_df$orbit <- substr(final_df$shot_number, 1, 4)
+n_orbit <- final_df %>% summarise(Unique_Elements = n_distinct(orbit))
+print(paste("Exporting", n_orbit, "orbits..."))
 
-# Count number of different final orbits
-nb_orbit <- final_df %>% summarise(Unique_Elements = n_distinct(orbit))
-
-# Add variable power to define full power and half power beam
-half_beam_names <- c("BEAM0011", "BEAM0010", "BEAM0001", "BEAM0000")
-final_df$power <- ifelse(final_df$beam_name %in% half_beam_names, "half", "full")
-
-# Save entire dataframe, and one for full beams only
-terra::saveRDS(final_df, file = "gedi_data.rds")
-
+saveRDS(final_df, file = "gedi_data.rds")
 if (out_vector) {
-  gdf <- terra::vect(final_df, geom = c("X_wgs", "Y_wgs"), crs = "EPSG:4326")
-  writeVector(gdf, "gedi_data_all.gpkg", overwrite = TRUE)
+  gdf <- vect(final_df, geom = c("x", "y"), crs = paste0("epsg:", epsg_code))
+  writeVector(gdf, "gedi_data.gpkg", overwrite = TRUE)
 }
